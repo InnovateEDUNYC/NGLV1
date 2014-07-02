29ce2d8a75f5a1b99baa43a92862dfd482173509
cameron-r <crowshanbin@gmail.com>
2014-07-02 11:15:15 -0500

    Valerie & Cameron | Added mapper for Session entity to Session view model

---
+++ src/NGL.Tests/Mappers/SessionTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using NGL.Web.Data.Entities;
+using NGL.Web.Models;
+using Shouldly;
+using Xunit;
+
+namespace NGL.Tests.Mappers
+{
+    public class SessionTest
+    {
+        [Fact]
+        public void ShouldMapSessionEntityToSessionViewModel()
+        {
+            SessionModel sessionModel = new SessionModel();
+            Session sessionEntity = new Session();
+            sessionEntity.SchoolId = 1;
+            sessionEntity.TermTypeId = 1;
+            sessionEntity.SchoolYear = 2014;
+            sessionEntity.SessionName = "Summer 2014";
+            sessionEntity.BeginDate = new DateTime(2014, 6, 26);
+            sessionEntity.EndDate = new DateTime(2014, 9, 26);
+            sessionEntity.TotalInstructionalDays = 92;
+
+            SessionToSessionModelMapper sessionToSessionModelMapper = new SessionToSessionModelMapper();
+            sessionToSessionModelMapper.Map(sessionEntity, sessionModel);
+
+            sessionModel.SchoolId.ShouldBe(1);
+            sessionModel.TermTypeId.ShouldBe(1);
+            sessionModel.SchoolYear.ShouldBe((short)2014);
+            sessionModel.SessionName.ShouldBe("Summer 2014");
+            sessionModel.BeginDate.ShouldBe(new DateTime(2014, 6, 26));
+            sessionModel.EndDate.ShouldBe(new DateTime(2014, 9, 26));
+            sessionModel.TotalInstructionalDays.ShouldBe(92);
+        }
+    }
+}
---
+++ src/NGL.Web/Models/Session/SessionToSessionModelMapper.cs
@@ -0,0 +1,20 @@
+﻿using NGL.Web.Data.Entities;
+using NGL.Web.Models.School;
+
+namespace NGL.Web.Models
+{
+    public class SessionToSessionModelMapper :
+        IMapper<Session, SessionModel>
+    {
+        public void Map(Session source, SessionModel target)
+        {
+            target.SchoolId = source.SchoolId;
+            target.TermTypeId = source.TermTypeId;
+            target.SchoolYear = source.SchoolYear;
+            target.SessionName = source.SessionName;
+            target.BeginDate = source.BeginDate;
+            target.EndDate = source.EndDate;
+            target.TotalInstructionalDays = source.TotalInstructionalDays;
+        }
+    }
+}
