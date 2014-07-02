5c0d9b7d7d0a27ab9da5826d70bd92e8de43cff6
Anirudh <rudysback@gmail.com>
2014-07-02 18:21:54 -0500

    #108 | Michael & Anirudh | Added lookup repository

---
+++ src/NGL.Web/Data/Repositories/LookupRepository.cs
@@ -0,0 +1,12 @@
+﻿using NGL.Web.Data.Infrastructure;
+
+namespace NGL.Web.Data.Repositories
+{
+    public class LookupRepository : GenericRepository, ILookupRepository
+    {
+        public LookupRepository(INglDbContext dbContext) : base(dbContext)
+        {
+
+        }
+    }
+}
