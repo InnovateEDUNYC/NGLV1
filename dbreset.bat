ad5def39bcd55dda358b29d22f0eae91bd0ff53c
MehdiK <me@mehdi-khalili.com>
2014-07-03 09:40:23 -0500

    dbreset can now close the connections

--- dbreset.bat
+++ dbreset.bat
@@ -1,4 +1,11 @@
+@echo off
+
+echo Kill IIS EXPRESS
+taskkill /IM IISEXPRESS*
+
 echo "Dropping NGL Database..."
+SqlCmd -Q "ALTER DATABASE [NGL] SET  SINGLE_USER WITH ROLLBACK IMMEDIATE"
+
 sqlcmd -Q "DROP DATABASE NGL;"
 
 echo "Creating NGL Database..."
