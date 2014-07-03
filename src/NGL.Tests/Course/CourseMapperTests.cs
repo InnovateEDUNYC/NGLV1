2d641aa0a88e05d610d324a0e3b70883c3176831
cameron-r <crowshanbin@gmail.com>
2014-07-03 14:48:39 -0500

    #145: added Course Entity to Course Model mapper and created Index view for Courses

---
+++ src/NGL.Tests/Course/CourseMapperTests.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using NGL.Web.Models.Course;
+using Shouldly;
+using Xunit;
+
+namespace NGL.Tests.Course
+{
+    public class CourseMapperTests
+    {
+
+        [Fact]
+        public void ShouldMapCourseEntityToCourseViewModel()
+        {
+            var courseModel = new CourseModel();
+            var courseEntity = new Web.Data.Entities.Course
+            {
+                CourseCode = "CSC101",
+                CourseTitle = "Intro to Programming",
+                NumberOfParts = 1,
+                AcademicSubjectDescriptorId = 12,
+                CourseDescription = "Learnin some Java",
+                DateCourseAdopted = new DateTime(2014, 07, 03),
+                HighSchoolCourseRequirement = false,
+                CourseGPAApplicabilityTypeId = 1,
+                CourseDefinedByTypeId = 1,
+                MinimumAvailableCreditTypeId = 1,
+                MinimumAvailableCreditConversion = new decimal(3.0),
+                MinimumAvailableCredit = new decimal(3.0),
+                MaximumAvailableCreditTypeId = 1,
+                MaximumAvailableCreditConversion = new decimal(3.0),
+                MaximumAvailableCredit = new decimal(3.0),
+                CareerPathwayTypeId = 1,
+                TimeRequiredForCompletion = 10,
+            };
+
+            var courseToCourseModelMapper = new CourseToCourseModelMapper();
+            courseToCourseModelMapper.Map(courseEntity, courseModel);
+
+            courseModel.CourseCode.ShouldBe("CSC101");
+            courseModel.CourseTitle.ShouldBe("Intro to Programming");
+            courseModel.NumberOfParts.ShouldBe(1);
+            courseModel.AcademicSubjectDescriptorId.ShouldBe(12);
+            courseModel.CourseDescription.ShouldBe("Learnin some Java");
+            courseModel.DateCourseAdopted.ShouldBe(new DateTime(2014, 07, 03));
+            courseModel.HighSchoolCourseRequirement.ShouldBe(false);
+            courseModel.CourseGPAApplicabilityTypeId.ShouldBe(1);
+            courseModel.CourseDefinedByTypeId.ShouldBe(1);
+            courseModel.MinimumAvailableCreditTypeId.ShouldBe(1);
+            courseModel.MinimumAvailableCreditConversion.ShouldBe(new decimal(3.0));
+            courseModel.MinimumAvailableCredit.ShouldBe(new decimal(3.0));
+            courseModel.MaximumAvailableCreditTypeId.ShouldBe(1);
+            courseModel.MaximumAvailableCreditConversion.ShouldBe(new decimal(3.0));
+            courseModel.MaximumAvailableCredit.ShouldBe(new decimal(3.0));
+            courseModel.CareerPathwayTypeId.ShouldBe(1);
+            courseModel.TimeRequiredForCompletion.ShouldBe(10);
+        }
+
+
+
+
+
+    }
+}
---
+++ src/NGL.Web/Controllers/CourseController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using NGL.Web.Data.Entities;
+using NGL.Web.Data.Infrastructure;
+using NGL.Web.Models;
+using NGL.Web.Models.Course;
+
+namespace NGL.Web.Controllers
+{
+    public partial class CourseController : Controller
+    {
+        private readonly IGenericRepository _genericRepository;
+        private readonly IMapper<Course, CourseModel> _courseToCourseModelMapper;
+
+        public CourseController(IGenericRepository genericRepository, IMapper<Course, CourseModel> courseToCourseModelMapper)
+        {
+            _genericRepository = genericRepository;
+            _courseToCourseModelMapper = courseToCourseModelMapper;
+        }
+
+
+        // GET: /Course
+        public virtual ActionResult Index()
+        {
+
+            IEnumerable<Course> courses = _genericRepository.GetAll<Course>();
+            var courseModels = new List<CourseModel>();
+
+            foreach (var course in courses)
+            {
+                var courseModel = new CourseModel();
+                _courseToCourseModelMapper.Map(course, courseModel);
+                courseModels.Add(courseModel);
+            }
+
+            return View(courseModels);
+        }
+    }
+}
