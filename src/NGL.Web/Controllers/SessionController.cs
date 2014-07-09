14c128c6e1833f4f4c2aedb357c17806c5484af8
cameron-r <crowshanbin@gmail.com>
2014-07-09 16:29:44 -0500

    #128 | Valerie & Cameron | Trying to create a duplicate session returns a validation error

--- src/NGL.Web/Controllers/SessionController.cs
+++ src/NGL.Web/Controllers/SessionController.cs
@@ -2,6 +2,7 @@
 using System.Web.Mvc;
 using NGL.Web.Data.Entities;
 using NGL.Web.Data.Infrastructure;
+using NGL.Web.Data.Queries;
 using NGL.Web.Models;
 using NGL.Web.Models.Session;
 
@@ -45,16 +46,35 @@ namespace NGL.Web.Controllers
         [HttpPost]
         public virtual ActionResult Create(CreateModel createModel)
         {
+            CheckIfExists(createModel);
             if (!ModelState.IsValid)
                 return View(createModel);
 
+
             var session = new Session();
             _createModelToEntityMapper.Map(createModel, session);
-            
             _genericRepository.Add(session);
             _genericRepository.Save();
 
             return RedirectToAction(Actions.Index());
+        }
+
+        private void CheckIfExists(CreateModel createModel)
+        {
+            var existingSession = _genericRepository.Get(new SessionByTermTypeAndSchoolYearQuery(
+                (int) createModel.Term,
+                (short) createModel.SchoolYear));
+
+            if (existingSession != null)
+            {
+                PutModelErrors();
+            }
+        }
+
+        private void PutModelErrors()
+        {
+            ModelState.AddModelError("term", " ");
+            ModelState.AddModelError("schoolYear", "This session already exists!");
         }
     }
 }
