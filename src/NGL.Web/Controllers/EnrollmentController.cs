ca25a6b1471efc33d201e8fb6588cd41477f10be
Mehdi Khalili <me@mehdi-khalili.com>
2014-07-14 14:54:14 -0500

    added Program Status page + Glimpse addon

--- src/NGL.Web/Controllers/EnrollmentController.cs
+++ src/NGL.Web/Controllers/EnrollmentController.cs
@@ -6,43 +6,46 @@ using NGL.Web.Models.Enrollment;
 
 namespace NGL.Web.Controllers
 {
-    public partial class EnrollmentController : Controller
-    {
-        private readonly IGenericRepository _repository;
-        private readonly IMapper<CreateStudentModel, Student> _enrollmentMapper;
+	public partial class EnrollmentController : Controller
+	{
+		private readonly IGenericRepository _repository;
+		private readonly IMapper<CreateStudentModel, Student> _enrollmentMapper;
 
-        public EnrollmentController(IGenericRepository repository, IMapper<CreateStudentModel, Student> enrollmentMapper)
-        {
-            _repository = repository;
-            _enrollmentMapper = enrollmentMapper;
-        }
+		public EnrollmentController(IGenericRepository repository, IMapper<CreateStudentModel, Student> enrollmentMapper)
+		{
+			_repository = repository;
+			_enrollmentMapper = enrollmentMapper;
+		}
 
-        //
-        // GET: /Enrollment/CreateStudent
-        public virtual ActionResult CreateStudent()
-        {
-            var enrollmentModel = new CreateStudentModel
-            {
-                ParentEnrollmentInfoModel = new ParentEnrollmentInfoModel()
-            };
-            return View(enrollmentModel);
-        }
+		//
+		// GET: /Enrollment/CreateStudent
+		public virtual ActionResult CreateStudent()
+		{
+			var enrollmentModel = new CreateStudentModel
+			{
+				ParentEnrollmentInfoModel = new ParentEnrollmentInfoModel()
+			};
+			return View(enrollmentModel);
+		}
 
-        // POST: /Enrollment/CreateStudent
-        [HttpPost]
-        public virtual ActionResult CreateStudent(CreateStudentModel createStudentModel)
-        {
-            if (ModelState.IsValid)
-            {
-                var student = new Student();
+		// POST: /Enrollment/CreateStudent
+		[HttpPost]
+		public virtual ActionResult CreateStudent(CreateStudentModel createStudentModel)
+		{
+			if (!ModelState.IsValid) 
+				return View(createStudentModel);
 
-                _enrollmentMapper.Map(createStudentModel, student);
-                _repository.Add(student);
-                _repository.Save();
-                return RedirectToAction("All", "Student");
-            }
+			var student = new Student();
 
-            return View(createStudentModel);
-        }
+			_enrollmentMapper.Map(createStudentModel, student);
+			_repository.Add(student);
+			_repository.Save();
+			return RedirectToAction(Actions.EnterProgramStatus(student.StudentUSI));
+		}
+
+		public virtual ActionResult EnterProgramStatus(int id)
+		{
+			return View();
+		}
 	}
 }
---
+++ src/NGL.Web/Models/Enrollment/ProgramStatusModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+using NGL.Web.Data.Entities;
+
+namespace NGL.Web.Models.Enrollment
+{
+    public class ProgramStatusModel
+    {
+        [Required]
+        public bool? TestingAccommodation { get; set; }
+        public string TestingAccommodationFile { get; set; }
+        [Required]
+        public bool? BilingualProgram { get; set; }
+        [Required]
+        public bool? EnglishAsSecondLanguage { get; set; }
+        [Required]
+        public bool? Gifted { get; set; }
+        [Required]
+        public bool? SpecialEducation { get; set; }
+        public string SpecialEducationFile { get; set; }
+        [Required]
+        public bool? TitleParticipation { get; set; }
+        public string TitleParticipationFile { get; set; }
+        [Required]
+        [Display(Name = "McKinney Vento")]
+        public bool? McKinneyVento { get; set; }
+        public string McKinneyVentoFileUrl { get; set; }
+        public SchoolFoodServicesEligibilityTypeEnum FoodServiceEligibilityStatus { get; set; }
+    }
+}
