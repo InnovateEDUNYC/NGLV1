b2df95cc859284a6d146fa6e01e17cad6266ff28
Valerie Roske <varoske@thoughtworks.com>
2014-07-15 16:00:58 -0500

    Valerie & Anirudh: added unit tests for class period fluent validation

---
+++ src/NGL.Tests/ClassPeriod/ValidationTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq.Expressions;
+using FluentValidation.TestHelper;
+using NGL.Web.Data.Infrastructure;
+using NGL.Web.Models.ClassPeriod;
+using NSubstitute;
+using Xunit;
+
+namespace NGL.Tests.ClassPeriod
+{
+    public class ValidationTests
+    {
+        [Fact]
+        public void ShouldReturnTrueIfClassPeriodDoesNotExist()
+        {
+            var genericRepository = Substitute.For<IGenericRepository>();
+            var classPeriodCreateModel = new CreateModel
+            {
+                ClassPeriodName = "Period 1"
+            };
+
+            var validator = new CreateModelValidator(genericRepository);
+
+            genericRepository
+                .Get(Arg.Any<Expression<Func<Web.Data.Entities.ClassPeriod, bool>>>())
+                .Returns(null as Web.Data.Entities.ClassPeriod);
+
+            validator.ShouldNotHaveValidationErrorFor(m => m.ClassPeriodName, classPeriodCreateModel.ClassPeriodName);
+        }
+
+        [Fact]
+        public void ShouldReturnFalseIfClassPeriodExists()
+        {
+            var genericRepository = Substitute.For<IGenericRepository>();
+            var classPeriodCreateModel = new CreateModel
+            {
+                ClassPeriodName = "Period 1"
+            };
+
+            var validator = new CreateModelValidator(genericRepository);
+
+            var classPeriodEntity = new Web.Data.Entities.ClassPeriod
+            {
+                ClassPeriodName = "Period 1"
+            };
+
+            genericRepository
+                .Get(Arg.Any<Expression<Func<Web.Data.Entities.ClassPeriod, bool>>>())
+                .Returns(classPeriodEntity);
+
+            validator.ShouldHaveValidationErrorFor(m => m.ClassPeriodName, classPeriodCreateModel.ClassPeriodName);
+        }
+    }
+}
