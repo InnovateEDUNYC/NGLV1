5c7af02039c66f4667f4bee80c4dcc317dd5f212
Valerie Roske <varoske@thoughtworks.com>
2014-07-15 21:16:00 -0500

    Valerie: added validation for locations

---
+++ src/NGL.Tests/Location/ValidationTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Linq.Expressions;
+using FluentValidation.TestHelper;
+using NGL.Web.Data.Infrastructure;
+using NGL.Web.Models.Location;
+using NSubstitute;
+using Xunit;
+
+namespace NGL.Tests.Location
+{
+    public class ValidationTests
+    {
+        [Fact]
+        public void ShouldReturnTrueIfLocationDoesNotExist()
+        {
+            var genericRepository = Substitute.For<IGenericRepository>();
+            var locationCreateModel = new CreateModel()
+            {
+                ClassroomIdentificationCode = "BKL200"
+            };
+
+            var validator = new CreateModelValidator(genericRepository);
+
+            genericRepository
+                .Get(Arg.Any<Expression<Func<Web.Data.Entities.Location, bool>>>())
+                .Returns(null as Web.Data.Entities.Location);
+
+            validator.ShouldNotHaveValidationErrorFor(m => m.ClassroomIdentificationCode, locationCreateModel.ClassroomIdentificationCode);
+            
+        }
+
+        [Fact]
+        public void ShouldReturnFalseIfLocationExists()
+        {
+            var genericRepository = Substitute.For<IGenericRepository>();
+            var locationCreateModel = new CreateModel()
+            {
+                ClassroomIdentificationCode = "BKL200"
+            };
+
+            var validator = new CreateModelValidator(genericRepository);
+
+            var locationEntity = new Web.Data.Entities.Location
+            {
+                ClassroomIdentificationCode = "BKL200"
+            };
+
+            genericRepository
+                .Get(Arg.Any<Expression<Func<Web.Data.Entities.Location, bool>>>())
+                .Returns(locationEntity);
+
+            validator.ShouldHaveValidationErrorFor(m => m.ClassroomIdentificationCode, locationCreateModel.ClassroomIdentificationCode);
+            
+        }
+       
+    }
+}
---
+++ src/NGL.Web/Models/Location/CreateModelValidator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FluentValidation;
+using NGL.Web.Data.Infrastructure;
+
+namespace NGL.Web.Models.Location
+{
+    public class CreateModelValidator : AbstractValidator<CreateModel>
+    {
+        private readonly IGenericRepository _genericRepository;
+
+        public CreateModelValidator(IGenericRepository genericRepository)
+        {
+            _genericRepository = genericRepository;
+            RuleFor(m => m.ClassroomIdentificationCode).Must(NotExist).WithMessage("This classroom location already exists.");
+        }
+
+        private bool NotExist(string classroomIdentificationCode)
+        {
+            if (string.IsNullOrEmpty(classroomIdentificationCode))
+                return true;
+
+            return
+                _genericRepository.Get<Data.Entities.Location>(
+                    l => l.ClassroomIdentificationCode == classroomIdentificationCode) == null;
+        }
+    }
+}
