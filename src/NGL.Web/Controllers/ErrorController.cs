adbec3a21739db0f280f3f9cbd1b28d097b56b75
Mehdi Khalili <me@mehdi-khalili.com>
2014-07-17 12:26:16 -0500

    Added error handing for mvc.

---
+++ src/NGL.Web/Controllers/ErrorController.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Mvc;
+using Antlr.Runtime.Misc;
+
+namespace NGL.Web.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ActionResult Index()
+        {
+            return Content("Generic Error");
+        }
+
+        public ActionResult HttpError404()
+        {
+            return Content("Caught a 404");
+        }
+
+        public ActionResult HttpError500()
+        {
+            return Content("Caught a 500");
+        }
+
+        public ActionResult General()
+        {
+            return Content("Caught a general http error");
+        }
+    }
+}
--- src/NGL.Web/Global.asax.cs
+++ src/NGL.Web/Global.asax.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
 using ChameleonForms;
+using Microsoft.Owin.Logging;
+using NGL.Web.Controllers;
 using StackExchange.Profiling;
 
 namespace NGL.Web
@@ -33,6 +37,57 @@ namespace NGL.Web
             {
                 MiniProfiler.Stop();
             }
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            Exception exception = Server.GetLastError();
+
+            Response.Clear();
+
+            var httpException = exception as HttpException;
+
+            var routeData = new RouteData();
+            routeData.Values.Add("controller", "Error");
+
+            if (httpException == null)
+            {
+                routeData.Values.Add("action", "Index");
+            }
+            else //It's an Http Exception, Let's handle it.
+            {
+                switch (httpException.GetHttpCode())
+                {
+                    case 404:
+                        // Page not found.
+                        routeData.Values.Add("action", "HttpError404");
+                        break;
+                    case 500:
+                        // Server error.
+                        routeData.Values.Add("action", "HttpError500");
+                        break;
+
+                    // Here you can handle Views to other error codes.
+                    // I choose a General error template  
+                    default:
+                        routeData.Values.Add("action", "General");
+                        break;
+                }
+            }
+
+            // Pass exception details to the target error View.
+            routeData.Values.Add("error", exception);
+
+            // Clear the error on server.
+            Server.ClearError();
+
+            // Avoid IIS7 getting in the middle
+            Response.TrySkipIisCustomErrors = true;
+
+            // Call target Controller and pass the routeData.
+            IController errorController = new ErrorController();
+            errorController.Execute(new RequestContext(
+                 new HttpContextWrapper(Context), routeData));
         }
     }
 }
