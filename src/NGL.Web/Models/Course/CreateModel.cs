63893281db0c8f951367a37b740e525e2c3ec98e
Valerie Roske <varoske@thoughtworks.com>
2014-07-18 15:29:46 -0500

    Valerie & Anirudh: added missing data annotations that cause website explosion

--- src/NGL.Web/Models/Course/CreateModel.cs
+++ src/NGL.Web/Models/Course/CreateModel.cs
@@ -7,24 +7,40 @@ namespace NGL.Web.Models.Course
     public class CreateModel
     {
         [Required]
+        [StringLength(60)]
         public string CourseCode { get; set; }
 
         [Required]
+        [StringLength(60)]
         public string CourseTitle { get; set; }
 
         [Required]
         public int NumberOfParts { get; set; }
         public AcademicSubjectDescriptorEnum? AcademicSubject { get; set; }
+
+        [StringLength(20)]
         public string CourseDescription { get; set; }
         public DateTime? DateCourseAdopted { get; set; }
         public bool? HighSchoolCourseRequirement { get; set; }
         public CourseGPAApplicabilityTypeEnum? CourseGPAApplicability { get; set; }
         public CourseDefinedByTypeEnum? CourseDefinedBy { get; set; }
         public CreditTypeEnum? MinimumAvailableCreditType { get; set; }
+
+        [RegularExpression(@"^\d+.\d{0,2}$")]
+        [Range(0, 9999999.99)]
         public decimal? MinimumAvailableCreditConversion { get; set; }
+
+        [RegularExpression(@"^\d+.\d{0,2}$")]
+        [Range(0, 9999999.99)]
         public decimal? MinimumAvailableCredit { get; set; }
         public CreditTypeEnum? MaximumAvailableCreditType { get; set; }
+
+        [RegularExpression(@"^\d+.\d{0,2}$")]
+        [Range(0, 9999999.99)]
         public decimal? MaximumAvailableCreditConversion { get; set; }
+
+        [RegularExpression(@"^\d+.\d{0,2}$")]
+        [Range(0, 9999999.99)]
         public decimal? MaximumAvailableCredit { get; set; }
         public CareerPathwayTypeEnum? CareerPathway { get; set; }
         public int? TimeRequiredForCompletion { get; set; }
--- src/NGL.Web/Models/Location/CreateModel.cs
+++ src/NGL.Web/Models/Location/CreateModel.cs
@@ -5,6 +5,7 @@ namespace NGL.Web.Models.Location
     public class CreateModel
     {
         [Required]
+        [StringLength(20)]
         public string ClassroomIdentificationCode { get; set; }
 
         public int? MaximumNumberOfSeats { get; set; }
