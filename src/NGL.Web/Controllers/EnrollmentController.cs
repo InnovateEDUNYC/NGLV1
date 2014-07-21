74ca9d14b751273e8c90342ebc7810afff9917ad
mlennon3 <mlennon3@gmail.com>
2014-07-21 18:11:59 -0500

    Updated the entities, fixed some redirect to error page issue

--- src/NGL.Web/Controllers/EnrollmentController.cs
+++ src/NGL.Web/Controllers/EnrollmentController.cs
@@ -55,7 +55,7 @@ namespace NGL.Web.Controllers
             var model = new AcademicDetailModel{StudentUsi = id};
             
             if (StudentDoesNotExist(id))
-                RedirectToAction(MVC.Error.General());
+                return RedirectToAction(MVC.Error.General());
 
             return View(model);
         }
@@ -93,7 +93,7 @@ namespace NGL.Web.Controllers
         public virtual ActionResult EnterProgramStatus(int id)
         {
             if (StudentDoesNotExist(id))
-                RedirectToAction(MVC.Error.General());
+               return RedirectToAction(MVC.Error.General());
             return View();
         }
 
