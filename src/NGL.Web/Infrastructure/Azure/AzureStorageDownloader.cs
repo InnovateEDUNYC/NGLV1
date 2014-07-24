44512f71be194eb13a0060489d486bed846d2c99
Valerie Roske <varoske@thoughtworks.com>
2014-07-23 20:11:33 -0500

    Valerie & Anirudh: create preliminary azure downloader

---
+++ src/NGL.Web/Infrastructure/Azure/AzureStorageDownloader.cs
@@ -0,0 +1,27 @@
+﻿using System.Web;
+using Microsoft.WindowsAzure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.Blob;
+
+namespace NGL.Web.Infrastructure.Azure
+{
+    public class AzureStorageDownloader
+    {
+        private CloudBlobClient _blobClient;
+
+        public AzureStorageDownloader()
+        {
+            CloudStorageAccount storageAccount = CloudStorageAccount.Parse(CloudConfigurationManager.GetSetting("BlobConnectionString"));
+            _blobClient = storageAccount.CreateCloudBlobClient();
+        }
+
+        public void Download(HttpPostedFileBase file, string container, string fileName)
+        {
+            var blobContainer = _blobClient.GetContainerReference(container);
+            var blockBlob = blobContainer.GetBlockBlobReference(fileName);
+            
+            blockBlob.Properties.ContentType = file.ContentType;
+            blockBlob.DownloadToStream(file.InputStream);
+        }
+    }
+}
