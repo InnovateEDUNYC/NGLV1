71975f4f3d1db33d91761f5df8629367bb43bc1b
Valerie Roske <varoske@thoughtworks.com>
2014-07-25 11:11:00 -0500

    Valerie & Anirudh: made create model for section

---
+++ src/NGL.Web/Controllers/SectionController.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+using NGL.Web.Data.Infrastructure;
+
+namespace NGL.Web.Controllers
+{
+    public partial class SectionController : Controller
+    {
+        private readonly IGenericRepository _genericRepository;
+
+        public SectionController(IGenericRepository genericRepository)
+        {
+            _genericRepository = genericRepository;
+        }
+
+        //
+        // GET: /Section/Create
+        public virtual ActionResult Create()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ src/NGL.Web/Models/Section/CreateModel.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NGL.Web.Models.Section
+{
+    public class CreateModel
+    {
+        [Required]
+        public string ClassPeriodName { get; set; }
+
+        [Required]
+        public string ClassroomIdentificationCode { get; set; }
+
+        [Required]
+        [StringLength(20)]
+        public string LocalCourseCode { get; set; }
+
+        [Required]
+        public int TermTypeId { get; set; }
+
+        [Required]
+        public short SchoolYear { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string UniqueSectionCode { get; set; }
+
+        [Required]
+        public int SequenceOfCourse { get; set; }
+
+    }
+}
