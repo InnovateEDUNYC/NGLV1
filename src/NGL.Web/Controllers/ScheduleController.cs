fd0a903021596cb0d19d0fdf43f826999ecd2b9d
mlennon3 <mlennon3@gmail.com>
2014-07-30 17:10:44 -0500

    Starting scheduling page

---
+++ src/NGL.Web/Controllers/ScheduleController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+using NGL.Web.Data.Entities;
+using NGL.Web.Models;
+using NGL.Web.Models.Schedule;
+
+namespace NGL.Web.Controllers
+{
+    public partial class ScheduleController : Controller
+    {
+        //
+        // GET: /Set/5
+        public virtual ActionResult Set(int id)
+        {
+//            var setModel = SetModel.CreateNewWith(student, sessionList);
+            return View(new SetModel());
+        }
+	}
+}
---
+++ src/NGL.Web/Models/Schedule/SetModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using ChameleonForms.Attributes;
+
+namespace NGL.Web.Models.Schedule
+{
+    public class SetModel
+    {
+        public SetModel()
+        {
+            this.Sessions = new List<SessionListItemModel>();
+        }
+        public string Name { get; set; }
+        public int StudentUsi { get; set; }
+        public string ProfilePhotoUrl { get; set; }
+//        public int SectionId { get; set; }
+//        public DateTime BeginDate { get; set; }
+//        public DateTime EndDate { get; set; }
+        public List<SessionListItemModel> Sessions { get; set; }
+
+        [ExistsIn("Sessions", "SessionId", "SessionName")]
+        public string Session { get; set; }
+    }
+
+}
