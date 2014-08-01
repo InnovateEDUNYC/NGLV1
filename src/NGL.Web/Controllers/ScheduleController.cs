bb83d5596f612c29917d0d3829a49f66bbdc0413
mlennon3 <mlennon3@gmail.com>
2014-08-01 17:31:38 -0500

    Can now associate a student with a section  using the form

--- src/NGL.Web/Controllers/ScheduleController.cs
+++ src/NGL.Web/Controllers/ScheduleController.cs
@@ -5,6 +5,7 @@ using System.Web.Mvc;
 using NGL.Web.Data.Entities;
 using NGL.Web.Data.Filters;
 using NGL.Web.Data.Infrastructure;
+using NGL.Web.Data.Repositories;
 using NGL.Web.Models;
 using NGL.Web.Models.Schedule;
 using NGL.Web.Models.Section;
@@ -15,16 +16,19 @@ namespace NGL.Web.Controllers
     public partial class ScheduleController : Controller
     {
         private readonly IGenericRepository _genericRepository;
+        private readonly ISchoolRepository _schoolRepository;
         private readonly ProfilePhotoUrlFetcher _profilePhotoUrlFetcher;
         private readonly IMapper<Session, SessionListItemModel> _sessionToSessionListItemModelMapper;
         private readonly IMapper<Section, AutocompleteModel> _sectionToAutocompleteModelMapper;
 
-        public ScheduleController(IGenericRepository genericRepository, 
+        public ScheduleController(IGenericRepository genericRepository,
+            ISchoolRepository schoolRepository,
             ProfilePhotoUrlFetcher profilePhotoUrlFetcher, 
             IMapper<Session, SessionListItemModel> sessionToSessionListItemModelMapper,
             IMapper<Section, AutocompleteModel> sectionToAutocompleteModelMapper )
         {
             _genericRepository = genericRepository;
+            _schoolRepository = schoolRepository;
             _profilePhotoUrlFetcher = profilePhotoUrlFetcher;
             _sessionToSessionListItemModelMapper = sessionToSessionListItemModelMapper;
             _sectionToAutocompleteModelMapper = sectionToAutocompleteModelMapper;
@@ -41,6 +45,32 @@ namespace NGL.Web.Controllers
             var defaultSessionListModel = _sessionToSessionListItemModelMapper.Build(new SessionFilter(_genericRepository).FindSession(DateTime.Now));
             var setModel = SetModel.CreateNewWith(student, profilePhotoUrl, sessions, defaultSessionListModel);
             return View(setModel);
+        }
+
+        //
+        // AJAX POST: /ScheduleStudent/
+        [HttpPost]
+        public virtual JsonResult ScheduleStudent(SetModel setModel)
+        {
+            var section = _genericRepository.Get<Section>(s => s.SectionIdentity == setModel.sectionId);
+            var schoolId = _schoolRepository.GetSchool().SchoolId;
+            var studentSectionAssociation = new StudentSectionAssociation
+            {
+                SchoolId = schoolId,
+                StudentUSI = setModel.StudentUsi,
+                BeginDate = setModel.BeginDate,
+                EndDate = setModel.EndDate,
+                SchoolYear = section.SchoolYear,
+                TermTypeId = section.TermTypeId,
+                LocalCourseCode = section.LocalCourseCode,
+                ClassPeriodName = section.ClassPeriodName,
+                ClassroomIdentificationCode = section.ClassroomIdentificationCode
+            };
+
+            _genericRepository.Add(studentSectionAssociation);
+            _genericRepository.Save();
+
+            return new JsonResult();
         }
 
         //
