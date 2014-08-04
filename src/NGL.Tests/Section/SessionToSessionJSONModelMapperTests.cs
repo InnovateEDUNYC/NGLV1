37fa8d12a3b151940e4d093b7c0ef3f2f5a2c94a
Valerie Roske <varoske@thoughtworks.com>
2014-08-04 16:38:08 -0500

    add sessionToSessionJSONModelMapperTest

---
+++ src/NGL.Tests/Section/SessionToSessionJSONModelMapperTests.cs
@@ -0,0 +1,23 @@
+﻿using NGL.Tests.Session;
+using NGL.Web.Models.Section;
+using Shouldly;
+using Xunit;
+
+namespace NGL.Tests.Section
+{
+    public class SessionToSessionJSONModelMapperTests
+    {
+        [Fact]
+        public void ShouldMap()
+        {
+            var entity = new SessionBuilder().Build();
+            var mapper = new SessionToSessionJSONModelMapper();
+
+            var model = mapper.Build(entity);
+
+            model.Term.ShouldBe(entity.TermTypeId);
+            model.SchoolYear.ShouldBe(entity.SchoolYear);
+            model.SessionName.ShouldBe("Fall Semester 2014");
+        }
+    }
+}
--- src/NGL.Web/Models/Section/SessionToSessionJSONModelMapper.cs
+++ src/NGL.Web/Models/Section/SessionToSessionJSONModelMapper.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using Humanizer;
+using NGL.Web.Data.Entities;
 
 namespace NGL.Web.Models.Section
 {
@@ -10,8 +8,8 @@ namespace NGL.Web.Models.Section
         public override void Map(Data.Entities.Session source, SessionJSONModel target)
         {
             target.SchoolYear = source.SchoolYear;
-            target.SessionName = source.SessionName;
             target.Term = source.TermTypeId;
+            target.SessionName = ((TermTypeEnum) source.TermTypeId).Humanize() + " " + source.SchoolYear;
         }
     }
 }
