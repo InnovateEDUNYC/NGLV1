3b1278f20075c4c3bd2f893242db3ca94be1eb31
mlennon3 <mlennon3@gmail.com>
2014-08-04 23:06:49 -0500

    SectionId is now hidden

--- src/NGL.Web/Assets/Scripts/Views/Schedule/setSchedule.js
+++ src/NGL.Web/Assets/Scripts/Views/Schedule/setSchedule.js
@@ -58,7 +58,7 @@ Ngl.schedule.setSchedule = (function () {
                         var beginDate = sectionListItem.BeginDate;
                         var endDate = sectionListItem.EndDate;
                         $('.current-section-list').append("<li class='current-section-list-item'>" +
-                            name + " " + beginDate + " - " + endDate + "<div class='section-id' id='section-id'>"+sectionListItem.SectionId+"</div></li>");
+                            name + " " + beginDate + " - " + endDate + "<div class='hidden section-id'>" + sectionListItem.SectionId + "</div></li>");
                     }
                 }
             });
