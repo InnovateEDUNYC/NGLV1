3a68ce9eed34c70098a9d7e0e68c9d652ec72e7f
Anirudh Dhullipalla <adhullip@thoughtworks.com>
2014-08-06 15:18:01 -0500

    Added script to add sample data to Assessment and AssessmentPerformanceLevel tables

---
+++ src/NGL.Web/Data/Scripts/012.1 - Add Assessment Sample Data.sample.sql
@@ -0,0 +1,46 @@
+﻿INSERT INTO [edfi].[Assessment]
+	  ([AssessmentTitle]
+      ,[AcademicSubjectDescriptorId]
+      ,[AssessedGradeLevelDescriptorId]
+      ,[Version]
+      ,[AssessmentCategoryTypeId])
+	VALUES
+	('Motor Skills Formative Assessment', 1, 119, 1, 17)
+
+
+declare @masteryTypeId int
+select @masteryTypeId = PerformanceBaseConversionTypeId from [edfi].[PerformanceBaseConversionType] where CodeValue = 'Mastery'
+
+declare @masteryDescriptorId int
+select @masteryDescriptorId = PerformanceLevelDescriptorId from [edfi].[PerformanceLevelDescriptor] where PerformanceBaseConversionTypeId = @masteryTypeId
+
+declare @nearMasteryTypeId int
+select @nearMasteryTypeId = PerformanceBaseConversionTypeId from [edfi].[PerformanceBaseConversionType] where CodeValue = 'Near Mastery'
+
+declare @nearMasteryDescriptorId int
+select @nearMasteryDescriptorId = PerformanceLevelDescriptorId from [edfi].[PerformanceLevelDescriptor] where PerformanceBaseConversionTypeId = @nearMasteryTypeId
+
+
+INSERT INTO [edfi].[AssessmentPerformanceLevel]
+           ([AssessmentTitle]
+		  ,[AcademicSubjectDescriptorId]
+		  ,[AssessedGradeLevelDescriptorId]
+		  ,[Version]
+		  ,[PerformanceLevelDescriptorId]
+		  ,[AssessmentReportingMethodTypeId]
+		  ,[MinimumScore]
+		  ,[ResultDatatypeTypeId])
+	VALUES
+	('Motor Skills Formative Assessment', 1, 119, 1, @masteryDescriptorId, 16, 90, 3)
+	
+INSERT INTO [edfi].[AssessmentPerformanceLevel]
+           ([AssessmentTitle]
+		  ,[AcademicSubjectDescriptorId]
+		  ,[AssessedGradeLevelDescriptorId]
+		  ,[Version]
+		  ,[PerformanceLevelDescriptorId]
+		  ,[AssessmentReportingMethodTypeId]
+		  ,[MinimumScore]
+		  ,[ResultDatatypeTypeId])
+	VALUES
+	('Motor Skills Formative Assessment', 1, 119, 1, @nearMasteryDescriptorId, 16, 70, 3)
