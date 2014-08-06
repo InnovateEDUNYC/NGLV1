d1ce9f0b78a7f44765d1d168ee847e91ad9d33e2
Aman Kumar <amkumar@thoughtworks.com>
2014-08-06 18:09:32 -0500

    Added assessment repo.

---
+++ src/NGL.Web/Data/Repositories/AssessmentRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using NGL.Web.Data.Entities;
+using NGL.Web.Data.Infrastructure;
+
+namespace NGL.Web.Data.Repositories
+{
+    public class AssessmentRepository : RepositoryBase, IAssessmentRepository
+    {
+        public AssessmentRepository(INglDbContext dbContext) : base(dbContext) { }
+
+        public IEnumerable<Assessment> GetAssessmentResults(int studentUsi, DateTime startDate, DateTime endDate)
+        {
+            return DbContext.Set<Assessment>()
+                .Where(a => a.AdministeredDate >= startDate && a.AdministeredDate <= endDate)
+                .Include(a => a.StudentAssessments.Select(sa => sa.StudentAssessmentScoreResults))
+                .Include(a => a.AssessmentPerformanceLevels)
+                .Include(a => a.AssessmentLearningStandards.Select(als => als.LearningStandard)).ToList();
+        }
+    }
+}
---
+++ src/NGL.Web/Data/Repositories/IAssessmentRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using NGL.Web.Data.Entities;
+
+namespace NGL.Web.Data.Repositories
+{
+    public interface IAssessmentRepository
+    {
+        IEnumerable<Assessment> GetAssessmentResults(int studentUsi, DateTime startDate, DateTime endDate);
+    }
+}
