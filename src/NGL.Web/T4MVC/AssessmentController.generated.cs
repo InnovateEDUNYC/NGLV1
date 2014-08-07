4eb6951a0afce47950d6e2106386ea73ab90abb7
Anirudh Dhullipalla <adhullip@thoughtworks.com>
2014-08-07 12:31:56 -0500

    Rebased

--- src/NGL.Web/T4MVC/AssessmentController.generated.cs
+++ src/NGL.Web/T4MVC/AssessmentController.generated.cs
@@ -60,6 +60,10 @@ namespace NGL.Web.Controllers
         {
             return new T4MVC_System_Web_Mvc_ActionResult(Area, Name, ActionNames.Result);
         }
+        public virtual System.Web.Mvc.ActionResult EnterResults()
+        {
+            return new T4MVC_System_Web_Mvc_ActionResult(Area, Name, ActionNames.EnterResults);
+        }
 
         [GeneratedCode("T4MVC", "2.0"), DebuggerNonUserCode]
         public AssessmentController Actions { get { return MVC.Assessment; } }
@@ -78,6 +82,7 @@ namespace NGL.Web.Controllers
         {
             public readonly string Create = "Create";
             public readonly string Result = "Result";
+            public readonly string EnterResults = "EnterResults";
         }
 
         [GeneratedCode("T4MVC", "2.0"), DebuggerNonUserCode]
@@ -85,6 +90,7 @@ namespace NGL.Web.Controllers
         {
             public const string Create = "Create";
             public const string Result = "Result";
+            public const string EnterResults = "EnterResults";
         }
 
 
@@ -105,6 +111,14 @@ namespace NGL.Web.Controllers
             public readonly string studentUsi = "studentUsi";
             public readonly string sessionId = "sessionId";
             public readonly string week = "week";
+        }
+        static readonly ActionParamsClass_EnterResults s_params_EnterResults = new ActionParamsClass_EnterResults();
+        [GeneratedCode("T4MVC", "2.0"), DebuggerNonUserCode]
+        public ActionParamsClass_EnterResults EnterResultsParams { get { return s_params_EnterResults; } }
+        [GeneratedCode("T4MVC", "2.0"), DebuggerNonUserCode]
+        public class ActionParamsClass_EnterResults
+        {
+            public readonly string assessmentId = "assessmentId";
         }
         static readonly ViewsClass s_views = new ViewsClass();
         [GeneratedCode("T4MVC", "2.0"), DebuggerNonUserCode]
@@ -166,97 +180,15 @@ namespace NGL.Web.Controllers
             return callInfo;
         }
 
-    }
-}
-
-#endregion T4MVC
-#pragma warning restore 1591
-        protected AssessmentController(Dummy d) { }
-
-        [GeneratedCode("T4MVC", "2.0"), DebuggerNonUserCode]
-        protected RedirectToRouteResult RedirectToAction(ActionResult result)
-        {
-            var callInfo = result.GetT4MVCResult();
-            return RedirectToRoute(callInfo.RouteValueDictionary);
-        }
-
-        [GeneratedCode("T4MVC", "2.0"), DebuggerNonUserCode]
-        protected RedirectToRouteResult RedirectToAction(Task<ActionResult> taskResult)
-        {
-            return RedirectToAction(taskResult.Result);
-        }
-
-        [GeneratedCode("T4MVC", "2.0"), DebuggerNonUserCode]
-        protected RedirectToRouteResult RedirectToActionPermanent(ActionResult result)
-        {
-            var callInfo = result.GetT4MVCResult();
-            return RedirectToRoutePermanent(callInfo.RouteValueDictionary);
-        }
-
-        [GeneratedCode("T4MVC", "2.0"), DebuggerNonUserCode]
-        protected RedirectToRouteResult RedirectToActionPermanent(Task<ActionResult> taskResult)
-        {
-            return RedirectToActionPermanent(taskResult.Result);
-        }
 
         [NonAction]
-        [GeneratedCode("T4MVC", "2.0"), DebuggerNonUserCode]
-        public virtual System.Web.Mvc.ActionResult EnterResults()
+        public override System.Web.Mvc.ActionResult Create(NGL.Web.Models.Assessment.CreateModel createModel)
         {
-            return new T4MVC_System_Web_Mvc_ActionResult(Area, Name, ActionNames.EnterResults);
+            var callInfo = new T4MVC_System_Web_Mvc_ActionResult(Area, Name, ActionNames.Create);
+            ModelUnbinderHelpers.AddRouteValues(callInfo.RouteValueDictionary, "createModel", createModel);
+            CreateOverride(callInfo, createModel);
+            return callInfo;
         }
-
-        [GeneratedCode("T4MVC", "2.0"), DebuggerNonUserCode]
-        public AssessmentController Actions { get { return MVC.Assessment; } }
-        [GeneratedCode("T4MVC", "2.0")]
-        public readonly string Area = "";
-        [GeneratedCode("T4MVC", "2.0")]
-        public readonly string Name = "Assessment";
-        [GeneratedCode("T4MVC", "2.0")]
-        public const string NameConst = "Assessment";
-
-        static readonly ActionNamesClass s_actions = new ActionNamesClass();
-        [GeneratedCode("T4MVC", "2.0"), DebuggerNonUserCode]
-        public ActionNamesClass ActionNames { get { return s_actions; } }
-        [GeneratedCode("T4MVC", "2.0"), DebuggerNonUserCode]
-        public class ActionNamesClass
-        {
-            public readonly string EnterResults = "EnterResults";
-        }
-
-        [GeneratedCode("T4MVC", "2.0"), DebuggerNonUserCode]
-        public class ActionNameConstants
-        {
-            public const string EnterResults = "EnterResults";
-        }
-
-
-        static readonly ActionParamsClass_EnterResults s_params_EnterResults = new ActionParamsClass_EnterResults();
-        [GeneratedCode("T4MVC", "2.0"), DebuggerNonUserCode]
-        public ActionParamsClass_EnterResults EnterResultsParams { get { return s_params_EnterResults; } }
-        [GeneratedCode("T4MVC", "2.0"), DebuggerNonUserCode]
-        public class ActionParamsClass_EnterResults
-        {
-            public readonly string assessmentId = "assessmentId";
-        }
-        static readonly ViewsClass s_views = new ViewsClass();
-        [GeneratedCode("T4MVC", "2.0"), DebuggerNonUserCode]
-        public ViewsClass Views { get { return s_views; } }
-        [GeneratedCode("T4MVC", "2.0"), DebuggerNonUserCode]
-        public class ViewsClass
-        {
-            static readonly _ViewNamesClass s_ViewNames = new _ViewNamesClass();
-            public _ViewNamesClass ViewNames { get { return s_ViewNames; } }
-            public class _ViewNamesClass
-            {
-            }
-        }
-    }
-
-    [GeneratedCode("T4MVC", "2.0"), DebuggerNonUserCode]
-    public partial class T4MVC_AssessmentController : NGL.Web.Controllers.AssessmentController
-    {
-        public T4MVC_AssessmentController() : base(Dummy.Instance) { }
 
         [NonAction]
         partial void EnterResultsOverride(T4MVC_System_Web_Mvc_ActionResult callInfo, int assessmentId);
