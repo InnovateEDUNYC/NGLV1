f6ff18a79819caa62ffe838eda54f6ef39c19fa2
Anirudh Dhullipalla <adhullip@thoughtworks.com>
2014-08-06 19:36:26 -0500

    Created simple enter results model for assessment

---
+++ src/NGL.Web/Models/Assessment/EnterResultsModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NGL.Web.Models.Assessment
+{
+    public class EnterResultsModel
+    {
+        public List<EnterResultsStudentModel> Students { get; set; }
+        public string AssessmentTitle { get; set; }
+
+    }
+
+}
---
+++ src/NGL.Web/Models/Assessment/EnterResultsStudentModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NGL.Web.Models.Assessment
+{
+    public class EnterResultsStudentModel
+    {
+        public string Name { get; set; }
+        public string StudentUsi { get; set; }
+        public decimal AssessmentResult { get; set; }
+    }
+}
