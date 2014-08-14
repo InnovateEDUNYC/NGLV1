ba7e778e8a044d6f6d78ad639bd4d425641a8227
Anirudh Dhullipalla <adhullip@thoughtworks.com>
2014-08-14 13:04:03 -0500

    fix ghost button issue

--- src/NGL.Web/Assets/Scripts/Views/Student/index.js
+++ src/NGL.Web/Assets/Scripts/Views/Student/index.js
@@ -1,6 +1,9 @@
 ﻿Ngl.createNS('Ngl.student.index');
 
 Ngl.student.index = (function () {
+    var setUpStudentProfile = function() {
+        profilePhotoButtons();
+    }
     var profilePhotoButtons = function () {
         $('.ghost-button').change(function() {
              $('form').submit();
