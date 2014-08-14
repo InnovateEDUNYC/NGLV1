2676a5e6dc7a0fc231b335f80ed1c67ce0e63c4a
Anirudh Dhullipalla <adhullip@thoughtworks.com>
2014-08-14 13:48:55 -0500

    implement arrow changes with accordion folding

--- src/NGL.Web/Assets/Scripts/Views/Student/index.js
+++ src/NGL.Web/Assets/Scripts/Views/Student/index.js
@@ -3,6 +3,7 @@
 Ngl.student.index = (function () {
     var setUpStudentProfile = function() {
         profilePhotoButtons();
+        accordianArrows();
     }
     var profilePhotoButtons = function () {
         $('.ghost-button').change(function() {
@@ -10,7 +11,19 @@ Ngl.student.index = (function () {
         });
     }
 
+    var accordianArrows = function() {
+        $(".panel").find("[data-toggle='collapse']").on("click", function() {
+            var arrowElement = $(this).find("i");
+            if (arrowElement.hasClass("fa-caret-down")) {
+                arrowElement.removeClass("fa-caret-down").addClass("fa-caret-up");
+            } else {
+                arrowElement.removeClass("fa-caret-up").addClass("fa-caret-down");
+            }
+
+        });
+    }
+
     return {
-        init: profilePhotoButtons
+        init: setUpStudentProfile
     }
 })();
