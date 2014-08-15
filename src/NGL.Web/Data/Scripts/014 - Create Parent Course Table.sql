e2e12e57c65113c03f44890b84f550e7f10f5cb3
Anirudh Dhullipalla <adhullip@thoughtworks.com>
2014-08-15 15:57:57 -0500

    make preliminary sql script for creating parent course table

---
+++ src/NGL.Web/Data/Scripts/014 - Create Parent Course Table.sql
@@ -0,0 +1,18 @@
+﻿IF NOT EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[edfi].[ParentCourse]') AND type in (N'U'))
+BEGIN
+CREATE TABLE [edfi].[ParentCourse](
+	[EducationOrganizationId] [int] NOT NULL,
+	[ParentCourseCode] [nvarchar](60) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+	[ParentCourseTitle] [nvarchar](60) COLLATE SQL_Latin1_General_CP1_CI_AS NOT NULL,
+	[ParentCourseDescription] [nvarchar](20) COLLATE SQL_Latin1_General_CP1_CI_AS NULL,
+	[Id] [uniqueidentifier] NOT NULL,
+	[LastModifiedDate] [datetime] NOT NULL,
+	[CreateDate] [datetime] NOT NULL,
+ CONSTRAINT [PK_Course] PRIMARY KEY CLUSTERED 
+(
+	[EducationOrganizationId] ASC,
+	[ParentCourseCode] ASC
+)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF)
+)
+END
+GO
