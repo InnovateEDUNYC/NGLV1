c6060dc7c6f55e2d96f560ae5c5854b92d777ddb
Anirudh Dhullipalla <adhullip@thoughtworks.com>
2014-08-18 17:41:38 -0500

    rename migration

--- src/NGL.Web/Models/ParentCourse/CreateModelToParentCourseMapper.cs
+++ src/NGL.Web/Models/ParentCourse/CreateModelToParentCourseMapper.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using NGL.Web.Data.Repositories;
+﻿using NGL.Web.Data.Repositories;
 
 namespace NGL.Web.Models.ParentCourse
 {
