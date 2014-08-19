a99c8a4464c06875dfedeffbaca7cfde5714cabc
Anirudh Dhullipalla <adhullip@thoughtworks.com>
2014-08-19 14:19:58 -0500

    readd validation

--- src/NGL.Web/Models/ParentCourse/CreateModel.cs
+++ src/NGL.Web/Models/ParentCourse/CreateModel.cs
@@ -1,9 +1,16 @@
-﻿namespace NGL.Web.Models.ParentCourse
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NGL.Web.Models.ParentCourse
 {
     public class CreateModel
     {
+        [Required]
+        [StringLength(60)]
         public string ParentCourseCode { get; set; }
+        [Required]
+        [StringLength(60)]
         public string ParentCourseTitle { get; set; }
+        [StringLength(20)]
         public string ParentCourseDescription { get; set; }
     }
 }
