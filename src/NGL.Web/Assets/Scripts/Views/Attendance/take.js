87902ef1aebeb7542876614b526b50ac274644a5
Aman Kumar <amkumar@thoughtworks.com>
2014-08-20 17:41:41 -0500

    Check for valid post on saving attendance. Enabled the unit test for TakeAttendanceModelMapper. Removed commented code.

--- src/NGL.Web/Assets/Scripts/Views/Attendance/take.js
+++ src/NGL.Web/Assets/Scripts/Views/Attendance/take.js
@@ -2,20 +2,33 @@
 
 Ngl.attendance.take = (function () {
     var init = function () {
-        var getStudentsUrlTemplate = "/Attendance/GetStudents?sectionId={sectionId}&sessionId={sessionId}&section={section}&session={session}&date={date}";
-
-        $("#get-students-btn").click(function () {
-            var url = getStudentsUrlTemplate
-                .replace("{sectionId}", $('#SectionId').val())
-                .replace("{sessionId}", $('#SessionId').val())
-                .replace("{section}", $('#Section').val())
-                .replace("{session}", $('#Session').val())
-                .replace("{date}", $('#Date').val());
-            window.location = url;
-        });
+        setUpEvents();
     }
+    var getStudentsUrlTemplate = "/Attendance/GetStudents?sectionId={sectionId}&sessionId={sessionId}&section={section}&session={session}&date={date}";
+
+    var getStudents = function () {
+        var url = getStudentsUrlTemplate
+            .replace("{sectionId}", $('#SectionId').val())
+            .replace("{sessionId}", $('#SessionId').val())
+            .replace("{section}", $('#Section').val())
+            .replace("{session}", $('#Session').val())
+            .replace("{date}", $('#Date').val());
+        window.location = url;
+    }
+
+    var clearStudentList = function() {
+        $(".attendance-list").hide();
+    }
+
+    var setUpEvents = function () {
+        $("#get-students-btn").click(getStudents);
+
+        $("#Date").on('change', clearStudentList);
+        $("#Session").on('change', clearStudentList);
+        $("#Section").on('change', clearStudentList);
+    };
 
     return {
         init: init
-        }
-})(); 
+    }
+})();
--- src/NGL.Web/Data/Repositories/ISectionRepository.cs
+++ src/NGL.Web/Data/Repositories/ISectionRepository.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using NGL.Web.Data.Entities;
+﻿using NGL.Web.Data.Entities;
 
 namespace NGL.Web.Data.Repositories
 {
     public interface ISectionRepository
     {
-        Section GetWithStudentAttendanceForDate(int sectionIdentity, DateTime dateTime);
+        Section GetWithStudentAttendance(int sectionIdentity);
     }
 }
