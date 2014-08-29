f210f30fd5cf23539648cc6a500e97f0042a6ce9
Valerie Roske <varoske@thoughtworks.com>
2014-08-29 11:36:11 -0500

    Corrected the Save button id while using the editHomeAddress.js

--- src/NGL.Web/Assets/Scripts/Views/Student/editHomeAddress.js
+++ src/NGL.Web/Assets/Scripts/Views/Student/editHomeAddress.js
@@ -3,7 +3,7 @@
 Ngl.student.editHomeAddress = (function () {
     var init = function () {
 
-    Ngl.shared.editProfile.setup('#readonly-home-address', '#collapseHomeAddress', '#editable-home-address > h4', '#editable-home-address > div', '#edit-home-address', '#save-home-address-edit', '#cancel-home-address-edit', '/student/editHomeAddress', "#edit-home-address-form");
+    Ngl.shared.editProfile.setup('#readonly-home-address', '#collapseHomeAddress', '#editable-home-address > h4', '#editable-home-address > div', '#edit-home-address', '#submit-home-address', '#cancel-home-address-edit', '/student/editHomeAddress', "#edit-home-address-form");
 
     }
 
