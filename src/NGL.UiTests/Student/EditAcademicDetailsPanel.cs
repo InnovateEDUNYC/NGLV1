36d07d598feda6ef34d23528d9d041dccae020c7
Valerie Roske <varoske@thoughtworks.com>
2014-09-02 16:11:39 -0500

    added ui test for editing academic details

---
+++ src/NGL.UiTests/Student/EditAcademicDetailsPanel.cs
@@ -0,0 +1,20 @@
+﻿using NGL.Web.Models.Enrollment;
+using OpenQA.Selenium;
+using TestStack.Seleno.PageObjects;
+
+namespace NGL.UiTests.Student
+{
+    public class EditAcademicDetailsPanel : Page<EditAcademicDetailModel>
+    {
+        public void Edit(EditAcademicDetailModel academicDetailModel)
+        {
+            Execute.Script("$('#AcademicDetail_MathScore').val('" + academicDetailModel.MathScore + "')");
+            Execute.Script("$('#AcademicDetail_ReadingScore').val('" + academicDetailModel.ReadingScore + "')");
+            Execute.Script("$('#AcademicDetail_WritingScore').val('" + academicDetailModel.WritingScore + "')");
+            Execute.Script("$('#AcademicDetail_PerformanceHistory').val('" + academicDetailModel.PerformanceHistory + "')");
+
+            Find.Element(By.Id("save-academic-details-edit")).Click();
+            WaitFor.AjaxCallsToComplete();
+        }
+    }
+}
