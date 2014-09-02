62e6004a08eb2d1f8891cf10eedbb67f62b189a8
Anirudh Dhullipalla <adhullip@thoughtworks.com>
2014-09-02 15:52:09 -0500

    When 'same address as student' is checked, address fields are cleared and disabled

--- src/NGL.Web/Assets/Scripts/Views/Student/editParentInfo.js
+++ src/NGL.Web/Assets/Scripts/Views/Student/editParentInfo.js
@@ -3,7 +3,37 @@
 Ngl.student.editParentInfo = (function () {
     var setUp = function() {
         Ngl.shared.editProfile.setup('#readonly-parent-info', '#collapseParentInfo', '#editable-parent-info > h4', '#editable-parent-info > div', '#edit-parent-info-button', '#save-parent-info-edit', '#cancel-parent-info-edit', '/student/editParentInfo', "#edit-parent-info-form");
+        setUpSameAddressCheckbox();
     }
+
+    var clearFieldsInElement = function (element) {
+        element.find("select, input[type='text']").each(function () { $(this).val(''); });
+    }
+
+    var setUpSameAddressCheckbox = function() {
+        $('#SameAddressAsStudent').on('click', function() {
+            clearFieldsInElement($('.edit-parent-address-section'));
+
+            if ($(this).is(':checked')) {
+                disableFieldsInElement($('.edit-parent-address-section'));
+            } else {
+                enableFieldsInElement($('.edit-parent-address-section'));
+            }
+        });
+    };
+
+    var disableFieldsInElement = function(element) {
+        element.find("select, input[type='text']").each(function() {
+            $(this).attr('disabled', 'disabled');
+        });
+    }
+
+    var enableFieldsInElement = function(element) {
+        element.find("select, input[type='text']").each(function () {
+            $(this).removeAttr('disabled');
+        });
+    }
+
 
     return {
         init: setUp
