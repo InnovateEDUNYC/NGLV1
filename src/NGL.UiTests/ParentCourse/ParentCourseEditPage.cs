10baaffdb15b2643b38da702280e989a034f7d8e
Valerie Roske <varoske@thoughtworks.com>
2014-09-02 15:45:09 -0500

    add ui test

---
+++ src/NGL.UiTests/ParentCourse/ParentCourseEditPage.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.UI;
+using NGL.Web.Models.ParentCourse;
+using OpenQA.Selenium;
+using TestStack.Seleno.PageObjects;
+
+namespace NGL.UiTests.ParentCourse
+{
+    public class ParentCourseEditPage : Page<EditModel>
+    {
+        public ParentCourseIndexPage EditParentCourse(EditModel editParentCourseModel)
+        {
+            Input.Model(editParentCourseModel);
+            return Navigate.To<ParentCourseIndexPage>(By.ClassName("btn")); 
+        }
+    }
+}
