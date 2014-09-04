2487e11141a7cd6b230c2f0c951fb6ab224e9e33
Valerie Roske <varoske@thoughtworks.com>
2014-09-04 11:35:56 -0500

    ui test for deleting a section

--- src/NGL.UiTests/Section/SectionIndexPage.cs
+++ src/NGL.UiTests/Section/SectionIndexPage.cs
@@ -1,4 +1,5 @@
-﻿using NGL.Web.Models.Section;
+﻿using System.Linq;
+using NGL.Web.Models.Section;
 using OpenQA.Selenium;
 using TestStack.Seleno.PageObjects;
 
@@ -17,6 +18,18 @@ namespace NGL.UiTests.Section
             Find.Element(By.CssSelector("tr:last-of-type td.section-code")).Text.Equals(createSectionModel.UniqueSectionCode);
 
             return sectionCodeExists;
+        }
+
+        public int GetNumberOfSections()
+        {
+            return Find.Elements(By.ClassName("delete-row-btn")).Count();
+        }
+
+        public SectionIndexPage GoDelete(CreateModel createSectionModel)
+        {
+            Execute.Script("$('[data-section=\"" + createSectionModel.Classroom + "-" + createSectionModel.Period + "-" + createSectionModel.Course + "-" + createSectionModel.Session + "\"]').find('.delete-row-btn').click()");
+
+            return this;
         }
     }
 }
