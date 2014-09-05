49fa90c0c3e6d948574e356014345b0c33767791
Valerie Roske <varoske@thoughtworks.com>
2014-09-05 10:23:32 -0500

    added small custom checkbox to Hispanic/Latino Ethnicity on edit biographical information

---
+++ src/NGL.Web/Assets/Scripts/Views/Shared/checkboxesAndRadioButtons.js
@@ -0,0 +1,22 @@
+﻿Ngl.createNS('Ngl.shared.checkboxesAndRadioButtons.js');
+
+Ngl.shared.checkboxesAndRadioButtons = (function () {
+    var init = function () {
+        checkRadioButton();
+    }
+
+    var checkRadioButton = function () {
+
+        $('input:checked + label > span').addClass('fa fa-check small-checkbox');
+        $('input').click(function () {
+            var spans = $(this).closest('tr').find('span');
+            spans.removeClass('fa fa-check');
+
+            $(this).next('label').children('span').addClass('fa fa-check small-checkbox');
+        });
+    }
+
+    return {
+        init: init
+    }
+})();
